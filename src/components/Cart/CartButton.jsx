92d5f00038a300dfc5ec0df486c8ffb4a0a02d17
Ruslan Stomatin <stomatinruslan@gmail.com>
2025-12-25 23:35:43 +0200

    feat: implement Order page with redux cart and private route

--- src/components/Cart/CartButton.jsx
+++ src/components/Cart/CartButton.jsx
@@ -1,17 +1,24 @@
-import { useCartCount } from "@/store/CartContext.jsx";
+import { useSelector } from "react-redux";
+import { selectCartCount } from "@/store/cart/cartSlice";
+import { Link } from "react-router-dom";
 import Button from "@/components/UI/Button/Button";
 import CartIcon from "@/assets/icons/Cart.svg";
 import s from "./CartButton.module.scss";
 
 export default function CartButton() {
-  const { count } = useCartCount();
+  const count = useSelector(selectCartCount);
 
   return (
     <div className={s.cartWrap}>
+      <Link to="/order">
       <Button className={s.big}>
         <img src={CartIcon} alt="" className={s.cart} aria-hidden />
       </Button>
-      <span className={s.badge} aria-live="polite">{count}</span>
+      </Link>
+
+      <span className={s.badge} aria-live="polite">
+        {count}
+      </span>
     </div>
   );
 }
