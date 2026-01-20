382ba9e5b660a9d32d256d9c2ea474fb6f4dbdaf
Ruslan Stomatin <stomatinruslan@gmail.com>
2026-01-20 19:44:07 +0200

    refactor(styles): rename OrderForm classes for readability

--- src/components/Order/OrderForm/OrderForm.tsx
+++ src/components/Order/OrderForm/OrderForm.tsx
@@ -3,14 +3,20 @@ import s from "@/components/Order/OrderForm/OrderForm.module.scss";
 export default function OrderForm() {
   return (
     <form className={s.orderForm}>
-      {/* This is a temporary solution adopted during the current sprint deadline. Refactoring will be performed shortly.  */}
-      <label className={s.label} htmlFor="street">Street</label>
-      <input className={s.input} id="street" placeholder="" />
+      {/* @TODO: This is a temporary solution adopted during the current sprint deadline. Refactoring will be performed shortly. */}
+      <label className={s.orderFormLabel} htmlFor="street">
+        Street
+      </label>
+      <input className={s.orderFormInput} id="street" placeholder="" />
 
-      <label className={s.label} htmlFor="house">House</label>
-      <input className={s.input} id="house" placeholder="" />
+      <label className={s.orderFormLabel} htmlFor="house">
+        House
+      </label>
+      <input className={s.orderFormInput} id="house" placeholder="" />
 
-      <button className={s.button} type="submit">Order</button>
+      <button className={s.orderFormSubmit} type="submit">
+        Order
+      </button>
     </form>
   );
 }
