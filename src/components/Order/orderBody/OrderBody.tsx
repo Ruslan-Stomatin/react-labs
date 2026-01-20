6bc17386809d9e1ca986da44ea9ea39a5b153eb9
Ruslan Stomatin <stomatinruslan@gmail.com>
2026-01-20 19:40:32 +0200

    refactor(styles): rename OrderBody classes for readability

--- src/components/Order/orderBody/OrderBody.tsx
+++ src/components/Order/orderBody/OrderBody.tsx
@@ -1,20 +1,20 @@
 import OrderList from "@/components/Order/OrderList/OrderList";
 import OrderSummary from "@/components/Order/OrderSummary/OrderSummary";
 import OrderForm from "@/components/Order/OrderForm/OrderForm";
-import s from "./OrderBody.module.scss"
+import s from "./OrderBody.module.scss";
 
 function OrderBody() {
-    return (
-        <section className={s.orderBody}>
-            <div className="container">
-                <h1>Finish your order</h1>
-                
-                <OrderList />
-                <OrderSummary />
-                <OrderForm />
-            </div>
-        </section>
-    )
+  return (
+    <section className={s.order}>
+      <div className="container">
+        <h1 className={s.orderTitle}>Finish your order</h1>
+
+        <OrderList />
+        <OrderSummary />
+        <OrderForm />
+      </div>
+    </section>
+  );
 }
 
 export default OrderBody;
