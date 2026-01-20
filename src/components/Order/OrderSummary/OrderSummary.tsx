9d338495faecd73ab89f1eff265883a30f94b2d6
Ruslan Stomatin <stomatinruslan@gmail.com>
2026-01-20 20:01:14 +0200

    refactor(styles): rename OrderSummary total class

--- src/components/Order/OrderSummary/OrderSummary.tsx
+++ src/components/Order/OrderSummary/OrderSummary.tsx
@@ -6,5 +6,9 @@ import s from "@/components/Order/OrderSummary/OrderSummary.module.scss";
 export default function OrderSummary() {
   const total = useSelector((state: RootState) => selectCartTotal(state));
 
-  return <h3 className={s.totalSum}>Total: ${total.toFixed(2)}</h3>;
+  return (
+    <h3 className={s.orderSummaryTotal}>
+      Total: ${total.toFixed(2)}
+    </h3>
+  );
 }
